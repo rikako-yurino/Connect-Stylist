51a023675ac450cc847c7dd690395f6c3f6bfbe7
rikako-yurino <dawnpurple.1989@gmail.com>
2021-04-08 19:24:31 +0900

    commit

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,5 @@
-const { environment } = require('@rails/webpack')
+const { environment } = require('@rails/webpacker')
+// const { environment } = require('@rails/webpack')
 const webpack = require('webpack')
 
 environment.plugins.prepend('Provide',
