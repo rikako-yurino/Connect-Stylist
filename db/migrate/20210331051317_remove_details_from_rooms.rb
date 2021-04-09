818744b6a5c815a38c058c7488913575ea14e2c6
rikako-yurino <dawnpurple.1989@gmail.com>
2021-04-09 16:19:00 +0900

    commit

--- db/migrate/20210331051317_remove_details_from_rooms.rb
+++
@@ -1,7 +0,0 @@
-class RemoveDetailsFromRooms < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :rooms, :purpose_id, :integer
-    remove_column :rooms, :style_id, :integer
-    remove_column :rooms, :remarks, :text
-  end
-end
