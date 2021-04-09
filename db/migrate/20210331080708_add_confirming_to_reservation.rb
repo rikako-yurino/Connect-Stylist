153c768e468ca4738a73d765dfdf3170fad36fb1
rikako-yurino <dawnpurple.1989@gmail.com>
2021-04-09 16:54:28 +0900

    commit

--- db/migrate/20210331080708_add_confirming_to_reservation.rb
+++
@@ -1,5 +0,0 @@
-class AddConfirmingToReservation < ActiveRecord::Migration[6.0]
-  def change
-    add_column :reservations, :confirming, :integer, null: false
-  end
-end
--- db/migrate/20210331090949_remove_confirming_from_reservations.rb
+++
@@ -1,5 +0,0 @@
-class RemoveConfirmingFromReservations < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :reservations, :confirming, :integer
-  end
-end
