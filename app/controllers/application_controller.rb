08be23d4c04d53ac7a5bc65dc13111e772807a23
rikako-yurino <dawnpurple.1989@gmail.com>
2021-04-11 13:15:08 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  http_basic_authenticate_with :name => ENV["BASIC_AUTH_NAME"], :password => ENV["BASIC_AUTH_PW"] if Rails.env.production?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
