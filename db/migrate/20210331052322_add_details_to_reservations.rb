90b895bc0d51512e496521a4e8590d2a476faeed
rikako-yurino <dawnpurple.1989@gmail.com>
2021-04-12 19:04:09 +0900

    commit

--- db/migrate/20210331052322_add_details_to_reservations.rb
+++ db/migrate/20210331052322_add_details_to_reservations.rb
@@ -1,7 +1,5 @@
 class AddDetailsToReservations < ActiveRecord::Migration[6.0]
   def change
-    add_column :reservations, :purpose_id, :integer
-    add_column :reservations, :style_id, :integer
-    add_column :reservations, :remarks, :text
+
   end
 end
